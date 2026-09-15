99607c1e606d9429e3f1a063aef0151ff28c23ae
estherjp88 <starburst8808@gmail.com>
2026-09-15 14:11:21 -0500

    Build Hero class

--- game.py
+++ game.py
@@ -4,7 +4,18 @@ from hero import Hero
 
 ARENA_NAME = "The Southlands"
 
+def battle(hero: Hero, enemy: Goblin):
+    while hero.isAlive() and enemy.is_alive():
+        heroDamage=hero.attack()
+        enemy.take_damage(heroDamage)
+        if enemy.is_alive():
+            enemy_damage=enemy.attack()
+            hero.takeDamage(enemy_damage)
 
+    if hero.isAlive():
+        print(f"{hero.name} wins")
+    else:
+        print(f"{enemy.name} wins")
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
@@ -25,7 +36,10 @@ def main():
 
     print(f"{hero.name} enters the arena with {hero.health} health.")
 
-    Goblin()
+    battle(hero, goblin)
+
+    
+
 
 
 if __name__ == "__main__":
